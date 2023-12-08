a55ad92c57c643e503e240a7573368ec95fc7c87
salino3 <programacionflavio@gmail.com>
2023-12-08 12:50:29 +0100

    add mockdata

---
+++ src/mock-data/section-info.ts
@@ -0,0 +1,149 @@
+export interface Vehicle {
+  car: Component;
+  moto: Component;
+  bicyle: Component;
+}
+
+export interface Component {
+  types: string[];
+  wheel: Brand[];
+  transmissionSystem: Brand[];
+  suspensionKit: Brand[];
+  brakes: Brand[];
+  engineModule?: Brand[];
+}
+
+export interface Brand {
+  [key: string]: string;
+}
+
+
+//
+
+
+export const vehicles: Vehicle = {
+  car: {
+    types: [
+      "Brecks",
+      "Engine Module",
+      "Wheels",
+      "Suspencion Kit",
+      "Transimissin System",
+    ],
+    brakes: [
+      { brembo: "Brembo" },
+      { ebc: "EBC Brakes" },
+      { hawk: "Hawk Performance" },
+      { bosch: "Bosch" },
+      { wilwood: "Wilwood" },
+    ],
+    engineModule: [
+      { motorCorp: "MotorCorp" },
+      { powerhouse: "Powerhouse Engines" },
+      { velocity: "Velocity Motors" },
+      { turboMax: "TurboMax" },
+      { forceDrive: "ForceDrive" },
+    ],
+    wheel: [
+      { pirelli: "Pirelli" },
+      { michelin: "Michelin" },
+      { continental: "Continental" },
+      { bridgestone: "Bridgestone" },
+      { goodyear: "Goodyear" },
+    ],
+    suspensionKit: [
+      { rideMaster: "RideMaster" },
+      { bilstein: "Bilstein" },
+      { kw: "KW Suspensions" },
+      { ohlins: "Öhlins" },
+      { koni: "KONI" },
+    ],
+    transmissionSystem: [
+      { shiftTech: "ShiftTech" },
+      { gearMasters: "GearMasters" },
+      { quickShift: "QuickShift Transmissions" },
+      { torqueDrive: "TorqueDrive" },
+      { precisionShift: "PrecisionShift" },
+    ],
+  },
+  moto: {
+    types: [
+      "Brecks",
+      "Engine Module",
+      "Wheels",
+      "Suspencion Kit",
+      "Transimissin System",
+    ],
+    brakes: [
+      { brembo: "Brembo" },
+      { ebc: "EBC Brakes" },
+      { hawk: "Hawk Performance" },
+      { bosch: "Bosch" },
+      { wilwood: "Wilwood" },
+    ],
+    engineModule: [
+      { motorCorp: "MotorCorp" },
+      { powerhouse: "Powerhouse Engines" },
+      { velocity: "Velocity Motors" },
+      { turboMax: "TurboMax" },
+      { forceDrive: "ForceDrive" },
+    ],
+    wheel: [
+      { pirelli: "Pirelli" },
+      { michelin: "Michelin" },
+      { continental: "Continental" },
+      { bridgestone: "Bridgestone" },
+      { goodyear: "Goodyear" },
+    ],
+    suspensionKit: [
+      { rideMaster: "RideMaster" },
+      { bilstein: "Bilstein" },
+      { kw: "KW Suspensions" },
+      { ohlins: "Öhlins" },
+      { koni: "KONI" },
+    ],
+    transmissionSystem: [
+      { shiftTech: "ShiftTech" },
+      { gearMasters: "GearMasters" },
+      { quickShift: "QuickShift Transmissions" },
+      { torqueDrive: "TorqueDrive" },
+      { precisionShift: "PrecisionShift" },
+    ],
+  },
+  bicyle: {
+    types: [
+      "Brecks",
+      "Wheels",
+      "Suspencion Kit",
+      "Transimissin System",
+    ],
+    brakes: [
+      { brembo: "Brembo" },
+      { ebc: "EBC Brakes" },
+      { hawk: "Hawk Performance" },
+      { bosch: "Bosch" },
+      { wilwood: "Wilwood" },
+    ],
+    wheel: [
+      { pirelli: "Pirelli" },
+      { michelin: "Michelin" },
+      { continental: "Continental" },
+      { bridgestone: "Bridgestone" },
+      { goodyear: "Goodyear" },
+    ],
+    suspensionKit: [
+      { rideMaster: "RideMaster" },
+      { bilstein: "Bilstein" },
+      { kw: "KW Suspensions" },
+      { ohlins: "Öhlins" },
+      { koni: "KONI" },
+    ],
+    transmissionSystem: [
+      { shiftTech: "ShiftTech" },
+      { gearMasters: "GearMasters" },
+      { quickShift: "QuickShift Transmissions" },
+      { torqueDrive: "TorqueDrive" },
+      { precisionShift: "PrecisionShift" },
+    ],
+  },
+};
